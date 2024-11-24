cd29be950f1f9c86406cde4a67feaf7993a5e1de
Garth天卜 <g-art-h@outlook.com>
2024-11-24 12:13:32 +0800

    添加帮助

--- FrameMark/MainWindow.xaml.cs
+++ FrameMark/MainWindow.xaml.cs
@@ -1,24 +1,14 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FrameMark
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// <summary> 主窗口的交互逻辑 </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        public MainWindow() => InitializeComponent();
+
+        private void MW_KeyDown(object sender, KeyEventArgs e)
+            => Components.Help.Show();
     }
 }
